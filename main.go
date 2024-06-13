dd7be7c1a41fee22e61053a87e58d949530680cd
Ebenezer Emmanuel Emonueje <eebenezer@gmail.com>
2024-06-13 22:30:28 +0100

    feat: return json

--- main.go
+++ main.go
@@ -3,11 +3,36 @@ package main
 import (
     "fmt"
     "net/http"
+	"encoding/json"
     "os"
 )
 
+type Message struct {
+    Text string `json:"message"`
+}
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Hello, World!")
+    // Create a Message struct instance
+    message := Message{
+        Text: "Hello, World!",
+    }
+
+    // Marshal the Message struct to JSON
+    jsonData, err := json.Marshal(message)
+    if err != nil {
+        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        return
+    }
+
+    // Set Content-Type header to application/json
+    w.Header().Set("Content-Type", "application/json")
+
+    // Write the JSON response
+    _, err = w.Write(jsonData)
+    if err != nil {
+        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        return
+    }
 }
 
 func main() {
