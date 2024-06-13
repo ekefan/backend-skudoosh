41ec796db06be06d5d53a9f247dd6aa25626a7ad
Ebenezer Emmanuel Emonueje <eebenezer@gmail.com>
2024-06-13 22:12:44 +0100

    fix: display

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
     "fmt"
     "net/http"
+    "os"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -10,7 +11,21 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+    // Retrieve port from environment variable, defaulting to 8080 if not set
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+
+    // Start server with specified host and port
+    host := os.Getenv("HOST")
+    if host == "" {
+        host = "0.0.0.0"
+    }
+
     http.HandleFunc("/", helloHandler)
-    fmt.Println("Starting server on :8080")
-    http.ListenAndServe(":8080", nil)
+    fmt.Printf("Starting server on %s:%s\n", host, port)
+    if err := http.ListenAndServe(host+":"+port, nil); err != nil {
+        fmt.Println("Error starting server:", err)
+    }
 }
